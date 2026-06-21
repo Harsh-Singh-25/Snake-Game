fa77a660612649f8ce1fd3380310b16e3033c448
Harsh Singh <harsh0singh25@gmail.com>
2026-06-21 18:02:05 +0530

    Reduce interval time for rendering from 300ms to 100ms

--- Script.js
+++ Script.js
@@ -123,7 +123,7 @@ function startTimer() {
 
 startButton.addEventListener("click", () => {
   modal.style.display = "none";
-  intervalId = setInterval(() => render(), 300);
+  intervalId = setInterval(() => render(), 100);
   startTimer();
 });
 
